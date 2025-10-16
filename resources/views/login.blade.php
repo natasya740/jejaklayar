3d2056680df5fdc4470175c3a6ed7bbee7973b2b
natasya <natasya7406@gmail.com>
2025-10-16 08:24:59 +0700

    perubahan login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body class="bg-yellow-100 flex items-center justify-center min-h-screen">
     <div class="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
-        <h2 class="text-2xl font-bold text-center mb-6 text-yellow-600">Masuk ke Jejak Layar</h2>
+        <h2 class="text-2xl font-bold text-center mb-6 text-yellow-600">Ayo ikut berkontribusi, data kamu berarti</h2>
 
         @if (session('error'))
             <div class="bg-red-100 text-red-700 p-3 rounded mb-4 text-sm">
